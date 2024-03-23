8016d8a73a8e3509b16ce228ef00915f6b588079
LeongWeiXin <wxleong.2020@scis.smu.edu.sg>
2024-03-23 17:04:56 +0800

    Update node3.py

--- main/node3.py
+++ main/node3.py
@@ -27,76 +27,8 @@ node2_ip = "0x2A"
 
 arp_mac = {node1_ip : router_mac, node2_ip : node2_mac}
 
-# thread1 = threading.Thread(target = send)
-# thread2 = threading.Thread(target = receive)
-# thread1.start()
-# thread2.start()
 
-while True:
-    # receive from router
-    received_message = node3.recv(1024)
-    received_message = received_message.decode("utf-8")
-
-    source_mac = received_message[0:2]
-    destination_mac = received_message[2:4]
-    source_ip = received_message[4:8]
-    destination_ip =  received_message[8:12]
-    protocol = received_message[12:14]
-    data_length = received_message[14:15]
-    data = received_message[15:]
-
-    # drop packet
-    if destination_mac != node3_mac:
-        print("\nPACKET DROPPED")
-    else:
-        print("\nINCOMING PACKET:")
-        print("Source MAC address: {source_mac} \nDestination MAC address: {destination_mac}".format(source_mac=source_mac, destination_mac=destination_mac))
-        print("Source IP address: {source_ip} \nDestination IP address: {destination_ip}".format(source_ip=source_ip, destination_ip=destination_ip))
-        print("Protocol: " + protocol)
-        print("Data length: " + data_length)
-        print("Data: " + data)
-
-        # ping reply, unicast -> reply to router and node2
-        if protocol == "0P":
-            protocol = "0R"
-            ethernet_header = node3_mac + arp_mac[source_ip]
-            IP_header = node3_ip + source_ip + protocol
-            packet = ethernet_header + IP_header + data_length + data
-            node3.send(bytes(packet, "utf-8"))
-            intra3.send(bytes(packet, "utf-8")) 
-
-    # receive from node2
-    received_message = intra3.recv(1024)
-    received_message = received_message.decode("utf-8")
-
-    source_mac = received_message[0:2]
-    destination_mac = received_message[2:4]
-    source_ip = received_message[4:8]
-    destination_ip =  received_message[8:12]
-    protocol = received_message[12:14]
-    data_length = received_message[14:15]
-    data = received_message[15:]
-
-    # drop packet
-    if destination_mac != node3_mac:
-        print("\nPACKET DROPPED")
-    else:
-        print("\nINCOMING PACKET:")
-        print("Source MAC address: {source_mac} \nDestination MAC address: {destination_mac}".format(source_mac=source_mac, destination_mac=destination_mac))
-        print("Source IP address: {source_ip} \nDestination IP address: {destination_ip}".format(source_ip=source_ip, destination_ip=destination_ip))
-        print("Protocol: " + protocol)
-        print("Data length: " + data_length)
-        print("Data: " + data)
-
-        # ping reply, unicast -> reply to router and node2
-        if protocol == "0P":
-            protocol = "0R"
-            ethernet_header = node3_mac + arp_mac[source_ip]
-            IP_header = node3_ip + source_ip + protocol
-            packet = ethernet_header + IP_header + data_length + data
-            node3.send(bytes(packet, "utf-8"))
-            intra3.send(bytes(packet, "utf-8")) 
-
+def send_outgoing_packet():
     # send new packet
     print("\nOUTGOING PACKET:")
     ethernet_header = ""
@@ -126,3 +58,74 @@ while True:
     node3.send(bytes(packet, "utf-8"))
     intra3.send(bytes(packet, "utf-8"))
 
+
+def receive_incoming_packet():   
+    while True:
+        # receive from router
+        received_message = node3.recv(1024)
+        received_message = received_message.decode("utf-8")
+
+        source_mac = received_message[0:2]
+        destination_mac = received_message[2:4]
+        source_ip = received_message[4:8]
+        destination_ip =  received_message[8:12]
+        protocol = received_message[12:14]
+        data_length = received_message[14:15]
+        data = received_message[15:]
+
+        # drop packet
+        if destination_mac != node3_mac:
+            print("\nPACKET DROPPED")
+        else:
+            print("\nINCOMING PACKET:")
+            print("Source MAC address: {source_mac} \nDestination MAC address: {destination_mac}".format(source_mac=source_mac, destination_mac=destination_mac))
+            print("Source IP address: {source_ip} \nDestination IP address: {destination_ip}".format(source_ip=source_ip, destination_ip=destination_ip))
+            print("Protocol: " + protocol)
+            print("Data length: " + data_length)
+            print("Data: " + data)
+
+            # ping reply, unicast -> reply to router and node2
+            if protocol == "0P":
+                protocol = "0R"
+                ethernet_header = node3_mac + arp_mac[source_ip]
+                IP_header = node3_ip + source_ip + protocol
+                packet = ethernet_header + IP_header + data_length + data
+                node3.send(bytes(packet, "utf-8"))
+                intra3.send(bytes(packet, "utf-8")) 
+
+        # receive from node2
+        received_message = intra3.recv(1024)
+        received_message = received_message.decode("utf-8")
+
+        source_mac = received_message[0:2]
+        destination_mac = received_message[2:4]
+        source_ip = received_message[4:8]
+        destination_ip =  received_message[8:12]
+        protocol = received_message[12:14]
+        data_length = received_message[14:15]
+        data = received_message[15:]
+
+        # drop packet
+        if destination_mac != node3_mac:
+            print("\nPACKET DROPPED")
+        else:
+            print("\nINCOMING PACKET:")
+            print("Source MAC address: {source_mac} \nDestination MAC address: {destination_mac}".format(source_mac=source_mac, destination_mac=destination_mac))
+            print("Source IP address: {source_ip} \nDestination IP address: {destination_ip}".format(source_ip=source_ip, destination_ip=destination_ip))
+            print("Protocol: " + protocol)
+            print("Data length: " + data_length)
+            print("Data: " + data)
+
+            # ping reply, unicast -> reply to router and node2
+            if protocol == "0P":
+                protocol = "0R"
+                ethernet_header = node3_mac + arp_mac[source_ip]
+                IP_header = node3_ip + source_ip + protocol
+                packet = ethernet_header + IP_header + data_length + data
+                node3.send(bytes(packet, "utf-8"))
+                intra3.send(bytes(packet, "utf-8")) 
+
+        send_outgoing_packet()  
+
+send_outgoing_packet()
+receive_incoming_packet()
